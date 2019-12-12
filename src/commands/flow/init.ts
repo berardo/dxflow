05c88b29542e005006d3872b86463e00bc2e1b76
Jose Berardo Cunha <joseberardo@gmail.com>
2019-12-12 15:15:52 +1100

    Init command

---
+++ src/commands/flow/init.ts
@@ -0,0 +1,66 @@
+import { flags, SfdxCommand } from '@salesforce/command';
+import { Messages } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+import { GitHandler, SfdxHandler, ConfigHandler } from '../../util';
+
+// Messaging
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('DXFlow', 'init');
+
+export default class Init extends SfdxCommand {
+  public static description = messages.getMessage('commandDescription');
+  public static examples = ['$ sfdx flow:init'];
+  public static args = [];
+  protected static requiresProject = false;
+  protected static supportsUsername = false;
+  protected static supportsDevhubUsername = false;
+  protected static flagsConfig = {
+    skipconfig: flags.boolean({
+      char: 's',
+      description: messages.getMessage('skipConfigFlagDescription'),
+    }),
+  };
+
+  public async run(): Promise<AnyJson> {
+    this.ux.log(messages.getMessage('logo'));
+
+    const configHandler = new ConfigHandler(this.ux);
+    const gitHandler = new GitHandler(this.ux);
+    const sfdxHandler = new SfdxHandler(this.ux);
+
+    if (!gitHandler.checkGitVersion()) return;
+    if (
+      !(await sfdxHandler.checkSfdxProject()) &&
+      (!(await sfdxHandler.wantToCreateNewSfdxProject()) ||
+        !(await sfdxHandler.createNewProject()))
+    ) {
+      return;
+    }
+
+    if (!gitHandler.checkGitStatus() && (await gitHandler.wantToGitInit())) {
+      gitHandler.createNewRepository();
+    }
+
+    const config = await configHandler.getGitConfig();
+
+    this.ux.startSpinner('creating branches');
+
+    gitHandler.createInitialCommit();
+
+    config.branches.forEach(branch => {
+      console.log(
+        `${branch.production}: ${gitHandler.createNewBranch(
+          branch.production,
+        )}`,
+      );
+      console.log(
+        `${branch.develop}: ${gitHandler.createNewBranch(branch.develop)}`,
+      );
+    });
+    gitHandler.checkoutBranch(config.branches[0].develop);
+
+    this.ux.stopSpinner('done');
+
+    return JSON.parse(JSON.stringify(config));
+  }
+}
